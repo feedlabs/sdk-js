19cba4f539f2d9cb18a9d389b158d3085a9039dc
Marian Sollmann <marian.sollmann@cargomedia.ch>
2014-02-28 10:41:01 +0100

    init

--- client.js
+++ client.js
@@ -2,30 +2,107 @@
 
 	var FeedMe = {
 
-		foo: 'foo',
+		options: {
+			foo: 'foo'
+		},
 
-		init: function(options, callbackStyler) {
-			this.foo = options.foo;
+		/**
+		 * @param {Object} options
+		 * @param {Function} callback
+		 */
+		init: function(options, callback) {
+			options = this._extend(this.options, options);
 
-			callbackStyler(options);
+			console.log(options);
+
+			callback(options);
 
 			// this.callbackStyler = callbackStyler
 			// connect to feedapi
 			// if succesfull then registerWebSocketHandle(this.refreshFeed)
 		},
 
-		registerWebSocketHandle: function(callback) {
-			// create webscoket connection
-			// on.('data', callback(data));
+		//so we expose a shout function
+		shout: function() {
+
+			console.log('shout');
+
+			//now we execute callbacks registered to shout
+			this._executeHandlers('shout');
 		},
 
-		registerAJAXHandle: function(callback) {
-			// create webSocket connection
-			// on.('data', callback(data));
+		//		registerWebSocketHandle: function(callback) {
+		//			create webscoket connection
+		//			on.('data', callback(data));
+		//		},
+
+		//		registerAJAXHandle: function(callback) {
+		//			create webSocket connection
+		//			on.('data', callback(data));
+		//		},
+
+		//		refreshFeed: function(data) {
+		//			this.callbackStyler(data);
+		//		},
+
+		/////////////////////////////////////////////////
+		/////////////////////////////////////////////////
+		/////////////////////////////////////////////////
+
+		//handler cache
+		handlers: {},
+
+		//our on function which collects handlers
+		on: function(eventName, handler) {
+			//if no handler collection exists, create one
+			if (!this.handlers[eventName]) {
+				this.handlers[eventName] = [];
+			}
+			this.handlers[eventName].push(handler);
 		},
 
-		refreshFeed: function(data) {
-			this.callbackStyler(data);
+		//our internal function that executes handlers with a given name
+		_executeHandlers: function(eventName) {
+			//get all handlers with the selected name
+			var handler = this.handlers[eventName] || []
+				, len = handler.length
+				, i;
+			//execute each
+			for (i = 0; i < len; i++) {
+				//you can use apply to specify what "this" and parameters the callback gets
+				handler[i].apply(this, []);
+			}
+		},
+
+		/////////////////////////////////////////////////
+		/////////////////////////////////////////////////
+		/////////////////////////////////////////////////
+
+
+		// Helper
+		////////////////////////////////////////////////
+
+		// merge objects into new object
+
+		/**
+		 * @param {Object} a
+		 * @param {Object} b
+		 * @returns {Object}
+		 * @private
+		 */
+		_extend: function(a, b) {
+			var c = {}, prop;
+			for (prop in a) {
+				if (a.hasOwnProperty(prop)) {
+					c[prop] = a[prop];
+				}
+			}
+			for (prop in b) {
+				if (b.hasOwnProperty(prop)) {
+					c[prop] = b[prop];
+				}
+			}
+			return c;
 		}
 
 	}; // FeedMe
@@ -40,4 +117,4 @@
 		window.FeedMe = FeedMe;
 	}
 
-}(this));
+}(window));
