30056e9d7b217b60aa82ea10f54c1578c80367dd
Marian Sollmann <marian.sollmann@cargomedia.ch>
2014-03-04 21:46:31 +0100

    add more logic

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 JS Client
----------
+=========
+
+The Javascript client will create a connection to the API "broadcaster" using individual ID's and token's.
+This ID's and token's will be generated bei the API system and related to each application registered at the API system.
+
+Workflow
+--------
+
+describe the workflow
+
+- init client
+ * id (user related)
+ * token (app related)
+ * styler callback function
+ * container (html wrapper)
+ * ...
+- register connection
+- get time line
+- print time line items
+
+### Methods
+
+- init
+- connect
+- style
+- print
+- reload
+- ...
+
--- client.js
+++ client.js
@@ -1,89 +1,153 @@
 (function(window) {
 
-	var MyPlugin = {
+  var MyPlugin = {
 
-		handlers: {},
-		options: {
-			foo: 'foo'
-		},
+    handlers: {},
+    outputContainer: null,
+    // default options
+    options: {
+      custId: '007',
+      token: 'demo123#',
+      outputContainerId: 'defaultContainerId'
+    },
+    timer: null,
+    timer_count: 0,
+    data: [
+      {type: 1, name: 'foo', params: 'bar'},
+      {type: 2, name: 'foo', params: 'bar'}
+    ],
 
-		/**
-		 * @param {Object} options
-		 * @param {Function} callback
-		 */
-		init: function(options, callback) {
-			options = this._extend(this.options, options);
+    /**
+     * @param {Object} options
+     * @param {Function|Null} stylerFunction
+     */
+    init: function(options, stylerFunction) {
+      options = this._extend(this.options, options);
+      this._stylerFunction = stylerFunction || this._stylerFunction;
 
-			// do stuff
+      this.outputContainer = document.getElementById(options.outputContainerId);
 
-			callback(options);
-		},
+      this._connectToServer(options);
 
-		// on function which collects handlers
-		on: function(eventName, handler) {
-			// if no handler collection exists, create one
-			if (!this.handlers[eventName]) {
-				this.handlers[eventName] = [];
-			}
-			this.handlers[eventName].push(handler);
-		},
+      this._executeHandlers('init');
+    },
 
-		//so we expose a shout function
-		shout: function() {
+    _connectToServer: function(options) {
+      // connect to server
+      // params (custId, token)
+      // if ok registerUser
 
-			console.log('hello from shout');
+      this._registerUser();
 
-			this._executeHandlers('shout');
-		},
+    },
 
-		// internal function that executes handlers with a given name
-		_executeHandlers: function(eventName) {
-			// get all handlers with the selected name
-			var handler = this.handlers[eventName] || []
-				, len = handler.length
-				, i;
-			// execute each
-			for (i = 0; i < len; i++) {
-				// use apply to specify what "this" and parameters the callback gets
-				handler[i].apply(this, []);
-			}
-		},
+    _registerUser: function() {
+      // ??? some webSocket stuff
+      // will use setInterval()
 
-		// Helpers
-		// =======
+      var _this = this;
 
-		/**
-		 * @param {Object} a
-		 * @param {Object} b
-		 * @returns {Object}
-		 * @private
-		 * merge objects into new object
-		 */
-		_extend: function(a, b) {
-			var c = {}, prop;
-			for (prop in a) {
-				if (a.hasOwnProperty(prop)) {
-					c[prop] = a[prop];
-				}
-			}
-			for (prop in b) {
-				if (b.hasOwnProperty(prop)) {
-					c[prop] = b[prop];
-				}
-			}
-			return c;
-		}
 
-	}; // MyPlugin
+      _this.printOutput(_this.data);
+      this.timer = setInterval(function() {
+        _this.timer_count += 1;
+        _this.printOutput(_this.data);
+      }, 10000);
 
-	// ===========================================================================
+    },
 
-	if ("function" === typeof define) {
-		define(function(require) {
-			return MyPlugin;
-		});
-	} else {
-		window.MyPlugin = MyPlugin;
-	}
+    /**
+     * @param {Object} data
+     */
+    printOutput: function(data) {
+      if (this.outputContainer) {
+        this.outputContainer.innerHTML = this._stylerFunction(data);
+      } else {
+        console.log('No `outputContainer` is defined. Add a container with `id=defaultContainerId` or pass a `outputContainerId` at init.');
+      }
+
+      this._executeHandlers('printOutput');
+    },
+
+    /**
+     * @param {String} eventName
+     * @param {Function} handler
+     * function which collects handlers
+     */
+    on: function(eventName, handler) {
+      // if no handler collection exists, create one
+      if (!this.handlers[eventName]) {
+        this.handlers[eventName] = [];
+      }
+      this.handlers[eventName].push(handler);
+    },
+
+    /**
+     * @param {Object} data
+     * @returns {String}
+     * @private
+     * default styler function
+     */
+    _stylerFunction: function(data) {
+      var html = '';
+      for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+          html += JSON.stringify(data[key]) + '<br>';
+        }
+      }
+      return html;
+    },
+
+    /**
+     * @param {String} eventName
+     * @private
+     * internal function that executes handlers with a given name
+     */
+    _executeHandlers: function(eventName) {
+      // get all handlers with the selected name
+      var handler = this.handlers[eventName] || [], len = handler.length, i;
+      // execute each
+      for (i = 0; i < len; i++) {
+        // use apply to specify what "this" and parameters the callback gets
+        handler[i].apply(this, []);
+      }
+    },
+
+    // Helpers
+    // =======
+
+    /**
+     * @param {Object} a
+     * @param {Object} b
+     * @returns {Object}
+     * @private
+     * merge objects into new object
+     */
+    _extend: function(a, b) {
+      var c = {}, prop;
+      for (prop in a) {
+        if (a.hasOwnProperty(prop)) {
+          c[prop] = a[prop];
+        }
+      }
+      for (prop in b) {
+        if (b.hasOwnProperty(prop)) {
+          c[prop] = b[prop];
+        }
+      }
+      return c;
+    }
+
+  }; // MyPlugin
+
+  // ===========================================================================
+
+  if ("function" === typeof define) {
+    define(function(require) {
+      return MyPlugin;
+    });
+  } else {
+    window.MyPlugin = MyPlugin;
+  }
 
 }(window));
