fe8a83f09a8f36f54003650177eeb66a35015027
Marian Sollmann <ms@sollmann-interactive.com>
2014-09-19 13:21:31 +0200

    make it working

--- feedify.js
+++ feedify.js
@@ -25,16 +25,11 @@
     /**
      * @param {Object|Null} options
      * @param {Function|Null} stylerFunction
-     * @param {Function|Null} updateFunction
      */
-    init: function(options, stylerFunction, updateFunction) {
+    init: function(options, stylerFunction) {
       this.options = this._extend(this.options, options);
-
       this._stylerFunction = stylerFunction || this._stylerFunction;
-      this._updateFunction = updateFunction || this._updateFunction;
-
       this.outputContainer = document.getElementById(this.options.outputContainerId);
-
       this._addDefaultEntries();
 
       var _this = this;
@@ -42,7 +37,7 @@
         FeedPlugin.load('http://www.feed.dev:10111/v1/feed/' + _this.options.feedId + '/entry', function(httpRequest) {
           _this._loadFirstEntries(JSON.parse(httpRequest.responseText));
         });
-      }, 3000);
+      }, 1500);
     },
 
     /**
@@ -53,16 +48,10 @@
       for (var key in firstEntries) {
         if (firstEntries.hasOwnProperty(key)) {
           var data = firstEntries[key];
-          var realData = JSON.parse(data.Data);
-          _this.processData(realData);
+          _this.add(data);
         }
       }
       this._removeDefaultEntries();
-    },
-
-    _helperToGetRealData: function(data) {
-      var realData = JSON.parse(data.Data);
-      return realData['data']['data'];
     },
 
     _addDefaultEntries: function() {
@@ -88,121 +77,58 @@
      * @param {Object} data
      */
     processData: function(data) {
-      this.add(data);
-      //if (data.action == 'add') {
-      //  this.add(data.clientData);
-      //} else if (data.action == 'remove') {
-      //  this.remove(data.clientData);
-      //} else if (data.action == 'update') {
-      //  this.update(data.clientData);
-      //}
-    },
-
-    /**
-     * @param {Object} clientData
-     */
-    add: function(clientData) {
-      var objectId = this._uniqueId();
-
-      this.objectList[objectId] = {
-        id: objectId,
-        clientId: clientData.id
-        //hoverTime: 0
-      };
-
-      // create new DOM object
-      var element = document.createElement('div');
-      element.id = objectId;
-      element.innerHTML = this._stylerFunction(clientData);
-
-      // prepend to list
-      this.outputContainer.insertBefore(element, this.outputContainer.firstChild);
-
-      // add listener
-      // var domObject = document.getElementById(objectId);
-      // domObject.addEventListener('mouseover', this._onMouseover, true);
-      // domObject.addEventListener('mouseout', this._onMouseout, true);
-    },
-
-    /**
-     * @param {Object} clientData
-     */
-    remove: function(clientData) {
-      var object = this.getObjectByClientId(clientData.id);
-      var domObject = document.getElementById(object.id);
-      // domObject.removeEventListener('mouseover', this._onMouseover, true);
-      // domObject.removeEventListener('mouseout', this._onMouseout, true);
-      domObject.remove();
-      delete this.objectList[object.id];
-    },
-
-    /**
-     * @param {Object} clientData
-     */
-    update: function(clientData) {
-      var object = this.getObjectByClientId(clientData.id);
-      this._updateFunction(object.id, clientData);
-    },
-
-    /**
-     * @param {String} id
-     * @returns {Object|Null}
-     */
-    getObjectById: function(id) {
-      if (undefined !== this.objectList[id]) {
-        return this.objectList[id];
+      switch (data.Action) {
+        case 'add':
+          this.add(data);
+          break;
+        case 'remove':
+          this.remove(data);
+          break;
+        case 'update':
+          this.update(data);
+          break;
+        default:
+          console.log('Unknown action `' + data.Action + '`');
       }
-      return null;
     },
 
     /**
-     * @param {String} id
-     * @returns {Object|Null}
+     * @param {Object} data
      */
-    getObjectByClientId: function(id) {
-      for (var key in this.objectList) {
-        if (this.objectList.hasOwnProperty(key)) {
-          var object = this.objectList[key];
-          if (object.clientId == id) {
-            return object;
-          }
-        }
-      }
-      return null;
+    add: function(data) {
+      var objectId = data.Id;
+      this.objectList[objectId] = objectId;
+
+      var domElement = document.createElement('div');
+      domElement.id = objectId;
+      domElement.innerHTML = this._stylerFunction(data.Data);
+
+      this.outputContainer.insertBefore(domElement, this.outputContainer.firstChild);
     },
 
-    //_onMouseover: function(event) {
-    //  var object = FeedPlugin.getObjectById(event.target.id);
-    //  object['mouseover'] = new Date().getTime();
-    //},
-    //
-    //_onMouseout: function(event) {
-    //  var object = FeedPlugin.getObjectById(event.target.id);
-    //  if (object.hasOwnProperty('mouseover')) {
-    //    var hoverTime = new Date().getTime() - object['mouseover'];
-    //    object.hoverTime += hoverTime;
-    //    console.log('"' + object.clientId + '" hover time: ' + hoverTime + '. Total hover time: ' + object.hoverTime);
-    //    delete object['mouseover'];
-    //  }
-    //},
+    /**
+     * @param {Object} data
+     */
+    remove: function(data) {
+      var domElement = document.getElementById(data.Id);
+      domElement.remove();
+      delete this.objectList[data.Id];
+    },
+
+    /**
+     * @param {Object} data
+     */
+    update: function(data) {
+      var domElement = document.getElementById(data.Id);
+      domElement.innerHTML = this._stylerFunction(data.Data);
+    },
 
     /**
      * @param {Object} data
      * @returns {String}
      */
     _stylerFunction: function(data) {
-      return JSON.stringify(data);
-    },
-
-    /**
-     * @param {String} domElementId
-     * @param {Object} data
-     * @returns {String}
-     */
-    _updateFunction: function(domElementId, data) {
-      console.log('No update function defined to process data: ');
-      console.log(data);
-      return '';
+      return JSON.stringify(data.Data);
     },
 
     // Helpers
@@ -292,161 +218,3 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
     }
   }
 };
-
-
-// old stuff
-
-
-//(function(window) {
-//
-//  var MyPlugin = {
-//
-//    handlers: {},
-//    outputContainer: null,
-//    // default options
-//    options: {
-//      custId: '007',
-//      token: 'demo123#',
-//      outputContainerId: 'defaultContainerId'
-//    },
-//    timer: null,
-//    timer_count: 0,
-//    data: [
-//      {type: 1, name: 'foo', params: 'bar'},
-//      {type: 2, name: 'foo', params: 'bar'}
-//    ],
-//
-//    /**
-//     * @param {Object} options
-//     * @param {Function|Null} stylerFunction
-//     */
-//    init: function(options, stylerFunction) {
-//      options = this._extend(this.options, options);
-//      this._stylerFunction = stylerFunction || this._stylerFunction;
-//
-//      this.outputContainer = document.getElementById(options.outputContainerId);
-//
-//      this._connectToServer(options);
-//
-//      this._executeHandlers('init');
-//    },
-//
-//    _connectToServer: function(options) {
-//      // connect to server
-//      // params (custId, token)
-//      // if ok registerUser
-//
-//      this._registerUser();
-//
-//    },
-//
-//    _registerUser: function() {
-//      // ??? some webSocket stuff
-//      // will use setInterval()
-//
-//      var _this = this;
-//
-//
-//      _this.printOutput(_this.data);
-//      this.timer = setInterval(function() {
-//        _this.timer_count += 1;
-//        _this.printOutput(_this.data);
-//      }, 10000);
-//
-//    },
-//
-//    /**
-//     * @param {Object} data
-//     */
-//    printOutput: function(data) {
-//      if (this.outputContainer) {
-//        this.outputContainer.innerHTML = this._stylerFunction(data);
-//      } else {
-//        console.log('No `outputContainer` is defined. Add a container with `id=defaultContainerId` or pass a `outputContainerId` at init.');
-//      }
-//
-//      this._executeHandlers('printOutput');
-//    },
-//
-//    /**
-//     * @param {String} eventName
-//     * @param {Function} handler
-//     * function which collects handlers
-//     */
-//    on: function(eventName, handler) {
-//      // if no handler collection exists, create one
-//      if (!this.handlers[eventName]) {
-//        this.handlers[eventName] = [];
-//      }
-//      this.handlers[eventName].push(handler);
-//    },
-//
-//    /**
-//     * @param {Object} data
-//     * @returns {String}
-//     * @private
-//     * default styler function
-//     */
-//    _stylerFunction: function(data) {
-//      var html = '';
-//      for (var key in data) {
-//        if (data.hasOwnProperty(key)) {
-//          html += JSON.stringify(data[key]) + '<br>';
-//        }
-//      }
-//      return html;
-//    },
-//
-//    /**
-//     * @param {String} eventName
-//     * @private
-//     * internal function that executes handlers with a given name
-//     */
-//    _executeHandlers: function(eventName) {
-//      // get all handlers with the selected name
-//      var handler = this.handlers[eventName] || [], len = handler.length, i;
-//      // execute each
-//      for (i = 0; i < len; i++) {
-//        // use apply to specify what "this" and parameters the callback gets
-//        handler[i].apply(this, []);
-//      }
-//    },
-//
-//    // Helpers
-//    // =======
-//
-//    /**
-//     * @param {Object} a
-//     * @param {Object} b
-//     * @returns {Object}
-//     * @private
-//     * merge objects into new object
-//     */
-//    _extend: function(a, b) {
-//      var c = {}, prop;
-//      for (prop in a) {
-//        if (a.hasOwnProperty(prop)) {
-//          c[prop] = a[prop];
-//        }
-//      }
-//      for (prop in b) {
-//        if (b.hasOwnProperty(prop)) {
-//          c[prop] = b[prop];
-//        }
-//      }
-//      return c;
-//    }
-//
-//  }; // MyPlugin
-//
-//  // ===========================================================================
-//
-//  if ("function" === typeof define) {
-//    define(function(require) {
-//      return MyPlugin;
-//    });
-//  } else {
-//    window.MyPlugin = MyPlugin;
-//  }
-//
-//}(window));
